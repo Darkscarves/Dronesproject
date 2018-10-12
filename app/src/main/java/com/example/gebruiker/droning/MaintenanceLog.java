d2c122dae484df1f465829a0e09b699001205cc9
darkscarves <barteverloo@gmail.com>
2018-10-12 09:49:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gebruiker/droning/MaintenanceLog.java
@@ -0,0 +1,20 @@
+package com.example.gebruiker.droning;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MaintenanceLog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maintenacelog);
+    }
+    public void toHome(View view)
+    {
+        Intent intent = new Intent(MaintenanceLog.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
