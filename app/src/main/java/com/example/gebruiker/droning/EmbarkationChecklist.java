6fc8f4c2634af6000b0a68dc870d6fadc1ee0144
geujevb12@gmail.com <Pukkie124>
2018-10-19 09:56:12 +0200

    embarkation toegevoegd still working on it!

---
+++ app/src/main/java/com/example/gebruiker/droning/EmbarkationChecklist.java
@@ -0,0 +1,20 @@
+package com.example.gebruiker.droning;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class EmbarkationChecklist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.embarkationchecklist);
+    }
+    public void toHome(View view)
+    {
+        Intent intent = new Intent(EmbarkationChecklist.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
