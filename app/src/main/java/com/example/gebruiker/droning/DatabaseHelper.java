69a58a1443d02824043d6d25b5023a83ed0db09a
darkscarves <barteverloo@gmail.com>
2018-11-27 11:14:03 +0100

    voortgang nog niet mergen

--- app/src/main/java/com/example/gebruiker/droning/DatabaseHelper.java
+++ app/src/main/java/com/example/gebruiker/droning/DatabaseHelper.java
@@ -23,6 +23,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             public static final String COL_12 = "ChargeInput";
             public static final String COL_13 = "FlightDuration";
             public static final String COL_14 = "PreFlight";
+            public static final String TABLE_NAME2 = "MaintenanceLog";
+            public static final String COL_15 = "Reason";
+            public static final String COL_16 = "WorkDone";
+            public static final String COL_17 = "PartsReplaced";
+            public static final String COL_18 = "SystemTestedYes";
+            public static final String COL_19 = "SystemTestedNo";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -33,12 +39,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NaamStudent TEXT, Datum DATE, IncidentTime TEXT, Damage TEXT, Details TEXT, ActionTaken TEXT, Notes TEXT )");
         db.execSQL("create table " + TABLE_NAME1 +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NaamStudent TEXT, Datum DATE, BatteryNo TEXT, BatteryResidual TEXT, ChargeDate TEXT, ChargeInput TEXT, FlightDuration TEXT, PreFlight TEXT, Notes TEXT )");
+        db.execSQL("create table " + TABLE_NAME2 +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NaamStudent TEXT, Datum DATE, Reason TEXT, WorkDone TEXT, PartsReplaced TEXT, SystemTested TEXT, Notes TEXT )");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME1);
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME2);
         onCreate(db);
     }
 
@@ -71,7 +79,24 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         contentValues.put(COL_13,FlightDuration);
         contentValues.put(COL_14,PreFlight);
         contentValues.put(COL_8,Notes);
-        long result = db.insert(TABLE_NAME,null,contentValues);
+        long result = db.insert(TABLE_NAME1,null,contentValues);
+        if (result == -1)
+            return false;
+        else
+            return true;
+    }
+    public boolean insertDataMaintenance(String NaamStudent, String Datum, String Reason, String WorkDone, String PartsReplaced, String SystemTestedYes,String SystemTestedNo, String Notes){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,NaamStudent);
+        contentValues.put(COL_3,String.valueOf(Datum));
+        contentValues.put(COL_15,Reason);
+        contentValues.put(COL_16,WorkDone);
+        contentValues.put(COL_17,PartsReplaced);
+        contentValues.put(COL_18,SystemTestedYes);
+        contentValues.put(COL_19,SystemTestedNo);
+        contentValues.put(COL_8,Notes);
+        long result = db.insert(TABLE_NAME2,null,contentValues);
         if (result == -1)
             return false;
         else
