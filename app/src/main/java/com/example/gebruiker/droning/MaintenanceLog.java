9e26084a97fbf02ca253ecae5c9167e614ffa7f0
darkscarves <barteverloo@gmail.com>
2018-11-27 10:03:15 +0100

    voortgang nog niet mergen

--- app/src/main/java/com/example/gebruiker/droning/MaintenanceLog.java
+++ app/src/main/java/com/example/gebruiker/droning/MaintenanceLog.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MaintenanceLog extends AppCompatActivity {
 
@@ -11,6 +15,35 @@ public class MaintenanceLog extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maintenancelog);
+        myDb = new DatabaseHelper(this);
+        StudentNaamEdit = (EditText)findViewById(R.id.StudentNaamEdit);
+        RedenEdit = (EditText)findViewById(R.id.RedenEdit);
+        PartsReplacedEdit = (EditText)findViewById(R.id.PartsReplacedEdit);
+        SystemTestedYes = (RadioButton)findViewById(R.id.SystemTestedYes);
+        SystemTestedNo = (RadioButton)findViewById(R.id.SystemTestedNo);
+        NotesEdit = (EditText)findViewById(R.id.NotesEdit);
+        SubmitBTN = (Button)findViewById(R.id.SubmitBTN);
+        AddData();
+    }
+    public void AddData() {
+        SubmitBTN.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData(StudentNaamEdit.getText().toString(),
+                                RedenEdit.getText().toString(),
+                                WorkDoneEdit.getText().toString(),
+                                PartsReplacedEdit.getText().toString(),
+                                SystemTestedYes.getText().toString(),
+                                SystemTestedNo.getText().toString(),
+                                NotesEdit.getText().toString() );
+                        if(isInserted =true)
+                            Toast.makeText(MaintenanceLog.this, "Data Inserted", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(MaintenanceLog.this, "Data not Inserted", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
     public void toHome(View view)
     {
