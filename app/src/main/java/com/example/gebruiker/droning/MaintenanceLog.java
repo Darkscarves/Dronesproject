0de11722f2a43dfdabfc5e7b2d69400a6245ea83
darkscarves <barteverloo@gmail.com>
2018-12-03 09:35:54 +0100

    voortgang presitesurvey

--- app/src/main/java/com/example/gebruiker/droning/MaintenanceLog.java
+++ app/src/main/java/com/example/gebruiker/droning/MaintenanceLog.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MaintenanceLog extends AppCompatActivity {
